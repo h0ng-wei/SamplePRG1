9f47310b9da630e90dbc9e6c620ce289c5dab64e
h0ng-wei <S10272098@connect.np.edu.sg>
2025-07-28 15:45:42 +0800

    This is my first commit

---
+++ GameScoreCalculator.py
@@ -0,0 +1,9 @@
+# Description: A. Gamer’s Final Score
+# Name: Lim Hong Wei
+# Student ID: 10272098K
+
+kill_count = 50 
+objective_completion = 80
+survival_time = 90 
+Final_performance_score = (30/100 * kill_count) + (30/100 * objective_completion) + (40/100 * survival_time) 
+print('Final performance score is', Final_performance_score)
