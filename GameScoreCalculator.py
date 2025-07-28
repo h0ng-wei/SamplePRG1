f2aad7c9fd4ae5535ccd95367cb81e4b79ad1027
h0ng-wei <S10272098@connect.np.edu.sg>
2025-07-28 15:50:14 +0800

    this is my second commit

--- GameScoreCalculator.py
+++ GameScoreCalculator.py
@@ -6,4 +6,5 @@ kill_count = 50
 objective_completion = 80
 survival_time = 90 
 Final_performance_score = (30/100 * kill_count) + (30/100 * objective_completion) + (40/100 * survival_time) 
-print('Final performance score is', Final_performance_score)
+print('Final performance score is', Final_performance_score) 
+k = 0
